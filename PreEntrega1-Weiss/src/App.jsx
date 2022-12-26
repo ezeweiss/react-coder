e930b4f16e85a3f895efb9a4e2785f00b0df3f09
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2022-12-26 12:41:52 -0300

    subo ItemListContainer

--- PreEntrega1-Weiss/src/App.jsx
+++ PreEntrega1-Weiss/src/App.jsx
@@ -1,12 +1,14 @@
 import { Button, ButtonGroup, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react'
 import './App.css'
+import ItemListContainer from './components/ItemListContainer'
 import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <div className="App">
-     <NavBar/>
+     <NavBar/><br/>     
+     <ItemListContainer greeting={"Bienvenido a la página de ShopStore!"}/>
     </div>
   )
 }
--- PreEntrega1-Weiss/src/components/ItemListContainer.jsx
+++ PreEntrega1-Weiss/src/components/ItemListContainer.jsx
@@ -1,8 +1,30 @@
+import { Card, CardBody, Divider, Image, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const ItemListContainer = () => {
+const ItemListContainer = ({greeting}) => {
   return (
-    <div>ItemListContainer</div>
+    <div>
+      {/* <Card>
+        <CardBody>
+          <Text>{greeting}</Text>
+        </CardBody>
+      </Card> */}
+      <Card maxW='sm'>
+  <CardBody>
+    <Image
+      src='https://www.cronica.com.ar/__export/1602295346282/sites/cronica/img/2020/10/09/homero1_crop1602295019493.jpg_1902800913.jpg'
+      alt='Green double couch with wooden legs'
+      borderRadius='lg'
+    />
+    <Stack mt='6' spacing='3'>
+      <Text>
+        {greeting}
+      </Text>
+    </Stack>
+  </CardBody>
+  <Divider />
+</Card>
+    </div>
   )
 }
 
